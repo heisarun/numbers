54a4130db9a75d367fe1c1123630af042a4c4cdd
heisarun <heisarun@gmx.com>
2025-12-23 20:53:41 +0530

    Update Jamroot.jam, Jamfile.jam, and 24 more files...

--- libraries/abstraction/abstraction.cpp
+++ libraries/abstraction/abstraction.cpp
@@ -1,6 +1,4 @@
 
-using namespace std;
-
 __declspec(dllexport) unsigned long long int abstraction(unsigned long long int first ,unsigned long long int second){
     return first + second ;
   }
--- libraries/modulus/modulusArithmetic.hpp
+++
@@ -1,2 +0,0 @@
-
-__declspec(dllimport) unsigned long long int modulus(unsigned long long int first ,unsigned long long int second);
